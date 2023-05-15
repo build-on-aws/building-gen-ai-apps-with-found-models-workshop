e727405f90b3123cb33a0e3f40472865d2cced8e
Obayomi <bobayomi@3c22fb0a93f3.ant.amazon.com>
2023-05-15 13:51:01 -0400

    init commit

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
-## My Project
+## Building Generative AI Applications with SageMaker Foundational Models
 
-TODO: Fill this README out!
+Welcome to our hands-on workshop on building generative AI applications using Amazon SageMaker and Jumpstart Notebooks with foundational models. In this interactive session, you will learn how to harness the power of AI to create innovative, scalable, and user-friendly solutions for text and image generation tasks. You will be building a tool that will enable content authors to generate attractive images and taglines for their articles.
 
-Be sure to:
+![The deployed app, that takes text from an article and creates a tagline with a cover image](./build_on_postly_example.png)
 
-* Change the title in this README
-* Edit your repository description on GitHub
+Throughout the workshop, we will guide you through three key steps to develop and deploy your application:
+
+1. Deploy a Foundational Model: We will walk you through using a SageMaker Jumpstart Notebook to deploy a state-of-the-art foundational model for text and image generation tasks.
+
+2. Integrate with AWS Lambda: In this step, you will learn how to create and deploy an AWS Lambda function that leverages the SageMaker model endpoint. This integration will enable seamless communication between your application and the deployed AI model, ensuring efficient processing of user inputs.
+
+3. Deploy a Front-End Application: Finally, we will demonstrate how to deploy a user-friendly front-end application that interacts with the deployed model endpoint. This application will allow users to experience the full potential of your AI-powered solution.
+
+By the end of this workshop, you will have gained valuable hands-on experience in deploying and integrating cutting-edge AI models with AWS services to build creative and engaging applications.
+
+Here is the link to the [workshop](https://catalog.workshops.aws/building-gen-ai-apps-with-found-models/en-US)
 
 ## Security
 
---
+++ fm_postly_public_st.py
@@ -0,0 +1,115 @@
+"""
+Purpose:
+    BuildOn Postly
+"""
+
+# 3rd party imports
+import streamlit as st
+import requests
+import numpy as np
+import os
+
+IMAGE_API = os.environ["IMAGE_API"]
+TEXT_API = os.environ["TEXT_API"]
+
+st.set_page_config(layout="wide")
+
+
+def gen_summary(text):
+    data = {"text": text}
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    resp = requests.post(TEXT_API, json=data, headers=headers)
+    # print(resp.json())
+    return resp.json()["text"]
+
+
+def gen_image(text):
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    data = {"text": text}
+
+    resp = requests.post(IMAGE_API, json=data, headers=headers)
+    # print(resp.json())
+
+    return np.array(resp.json()["generated_image"])
+
+
+def app() -> None:
+    """
+    Purpose:
+        Controls the app flow
+    Args:
+        N/A
+    Returns:
+        N/A
+    """
+    col1, col2 = st.columns(2)
+
+    article = col1.text_area("Input article", height=900)
+
+    styles = col2.multiselect(
+        "Image Style",
+        [
+            "Cloud",
+            "Pixel Art",
+            "Landscape",
+            "Cityscape",
+            "Futuristic",
+            "Watercolor",
+            "Technology",
+            "Computer",
+            "Cyberpunk",
+            "Realistic",
+            "Retro",
+            "Abstract",
+            "Geometric",
+            "Network",
+        ],
+    )
+
+    if col2.button("Generate"):
+        with st.spinner("In progress..."):
+            summary_text = gen_summary(article)
+
+            style_string = ""
+            for style in styles:
+                style_string += style + " "
+
+            summary_image = gen_image(summary_text + style_string)
+
+            col2.subheader(summary_text)
+            col2.image(summary_image)
+
+
+def main() -> None:
+    """
+    Purpose:
+        Controls the flow of the streamlit app
+    Args:
+        N/A
+    Returns:
+        N/A
+    """
+
+    # Start the streamlit app
+
+    st.markdown(
+        "<h1 style='text-align: center;'>BuildOn Postly</h1>", unsafe_allow_html=True
+    )
+
+    st.markdown(
+        "<h3 style='text-align: center;'>Sharing your article or idea with the world? Make your content pop with a great tagline and image</h3>",
+        unsafe_allow_html=True,
+    )
+
+    app()
+
+
+if __name__ == "__main__":
+    main()
